c05e02ed0f906a8ec8a9167871d43b14aeefb1e5
Andrew Mejia <amejia49@gmail.com>
2017-01-16 15:22:16 -0500

    Set up file structure

---
+++ app/components/Home.js
@@ -0,0 +1,9 @@
+import React, { PropTypes, Component } from 'react'
+
+export default function Home () {
+  return (
+    <div>
+      <p> Home </p>
+    </div>
+  )
+}
---
+++ app/redux/modules/feed.js
@@ -0,0 +1,11 @@
+
+const initialState = {
+  userFeed:{}
+}
+export default function feed(state=initialState, action) {
+  switch(action.type){
+    default:
+      return state
+  }
+
+}
