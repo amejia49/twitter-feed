9e25b562685bac4988926a4598ae6432fb90b769
Andrew Mejia <amejia49@gmail.com>
2017-01-16 16:51:30 -0500

    installed node-twitter package and set streaming listener

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
-var path = require('path');
-var express = require('express');
-var app = new express();
+var path = require('path')
+var express = require('express')
+var Twitter = require('twitter')
+var app = new express()
 var port = process.env.PORT || 5000;
 
 app.use(express.static(path.join(__dirname, 'dist')));
@@ -13,6 +14,25 @@ app.listen(port, function(error) {
   if (error) {
     console.error(error);
   } else {
+    console.log('process', process.env.hey)
     console.info('==> 🌎  Listening on port %s. Open up http://localhost:%s/ in your browser.', port, port);
   }
+});
+
+var twitter = new Twitter({
+  consumer_key: process.env.TWITTER_CONSUMER_KEY,
+  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+  access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+});
+
+twitter.stream('statuses/filter', {track: 'javascript'}, function(stream) {
+  stream.on('data', function(event) {
+    console.log('event', event)
+    console.log(event && event.text);
+  });
+
+  stream.on('error', function(error) {
+    throw error;
+  });
 });
