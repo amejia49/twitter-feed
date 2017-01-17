ac18d7b66f08848de5d35f277fa9f61611bdfc90
Andrew Mejia <amejia49@gmail.com>
2017-01-17 03:38:51 -0500

    filtered

--- config/twitter.js
+++
@@ -1,8 +0,0 @@
-var config = {
-  consumer_key: 'LQ9TpaeZjAjgWG59tG40zDW3t',
-  consumer_secret: 'bvsO6qJmRl3T99U8U4wQab6nQlwI8Jk0IFEJwgI8mi4CZos5gU',
-  access_token_key: '821103193190961157-tiV3sRS0DGNfsVm3Mqz017O4TxBUzrA',
-  access_token_secret: 'BEpalX0SmZDMA7oLcTZB5tEJ2GWWBlHkBspofG8IVaoCs'
-};
-
-module.exports = config
